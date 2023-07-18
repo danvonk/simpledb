3bc1a6611613f161df0d2e7f836482a93dccb07b
Daniel Vonk <daniel.vonk@kdab.com>
2023-07-18 17:30:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# simpledb
+[![simpledb on hackage](https://img.shields.io/hackage/v/simpledb)](http://hackage.haskell.org/package/simpledb)
+[![simpledb on Stackage Nightly](https://stackage.org/package/simpledb/badge/nightly)](https://stackage.org/nightly/package/simpledb)
+
+Generated with [template-haskell](https://github.com/jonascarpay/template-haskell)
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "simpledb";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs:
+    let
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              simpledb = hfinal.callCabal2nix "simpledb" ./. { };
+            };
+        };
+        simpledb = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.simpledb;
+      };
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.simpledb ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+            ];
+          };
+          defaultPackage = pkgs.simpledb;
+        };
+    in
+    { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
